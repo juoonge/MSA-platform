23ccd14d8b82f8254efcd78d2de504a1b94efc0b
Mcgeolypazun <taratula0504@gmail.com>
2024-09-10 11:03:34 +0900

    feat : eureka 서버 설정

--- server/src/main/java/com/example/sparta/msa_platform/MsaPlatformApplication.java
+++ server/src/main/java/com/example/sparta/msa_platform/MsaPlatformApplication.java
@@ -2,8 +2,10 @@ package com.example.sparta.msa_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsaPlatformApplication {
 
 	public static void main(String[] args) {
