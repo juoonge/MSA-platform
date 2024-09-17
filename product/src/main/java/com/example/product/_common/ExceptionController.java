e9544437dffa80333ace419115f654080a5ddbe0
Panjun-Kwon <pndlm1979@naver.com>
2024-09-17 20:19:31 +0900

    refacotring product

--- product/src/main/java/com/example/product/_common/ExceptionController.java
+++ product/src/main/java/com/example/product/_common/ExceptionController.java
@@ -1,19 +1,46 @@
 package com.example.product._common;
 
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import feign.*;
+import lombok.*;
+import lombok.extern.slf4j.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
+@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ExceptionController {
 
+    private final ObjectMapper objectMapper;
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<ApiResponse> feignException(FeignException e) throws JsonProcessingException {
+        log.warn(e.getMessage());
+        String responseJson = e.contentUTF8();
+        Map<String, String> responseMap = objectMapper.readValue(responseJson, Map.class);
+        String message = responseMap.get("message");
+        log.info("400 나올 껄?");
+        log.info(responseMap.get("statusCode"));
+        log.info("error 날 껄?");
+        responseMap.get("statusCode");
+        log.info("났나?");
+        return new ResponseEntity<>(ApiResponse.fail(message), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(ApiException.class)
-    public static ResponseEntity<ApiResponse> apiException(ApiException e) {
+    public ResponseEntity<ApiResponse> apiException(ApiException e) {
+        log.warn(e.getMessage());
         return new ResponseEntity<>(ApiResponse.fail(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public static ResponseEntity<ApiResponse> exception(Exception e) {
-        return new ResponseEntity<>(ApiResponse.error(e.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponse> exception(Exception e) {
+        log.warn(e.getMessage());
+        return new ResponseEntity<>(ApiResponse.fail(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
